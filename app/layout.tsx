408da8415157bcaf9851c5d56e9cda0088205aef
heynzar <heynzar@gmail.com>
2026-04-23 12:16:00 +0100

    feat: update layout and page structure, add Toaster component, and implement theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,26 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/provider/theme-provider"
+import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/sonner"
+import { Metadata } from "next"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 })
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s ✦ CleanSlides",
+    default: "CleanSlides ✦ AI Presentations Builder",
+  },
+
+  description: "Produce clean, porfessional presentations [PPT on Steroids]",
+}
 
 export default function RootLayout({
   children,
@@ -20,10 +31,18 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   )
