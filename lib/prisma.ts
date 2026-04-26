a714fd77bf0196c0c439f9db9781e432b55b9621
heynzar <heynzar@gmail.com>
2026-04-26 15:11:22 +0100

    refactor: remove unused Prisma adapter and related dependencies

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
 
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
-
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
 export const client = globalThis.prisma || new PrismaClient()
 
